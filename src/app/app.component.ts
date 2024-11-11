4733e5dd18c949c4ea227cfb716e350dac6dffbc
1564912sergi <sergi15lopez@gmail.com>
2024-11-11 17:51:55 +0100

    refactor: update main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { HomeSectionComponent } from './components/home-section/home-section.component';
+import { CardCarouselComponent } from './components/card-carousel/card-carousel.component';
+import { ProjectCardComponent } from './components/project-card/project-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    SidebarComponent,
+    CommonModule,
+    HomeSectionComponent,
+    CardCarouselComponent,
+    ProjectCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'portfolio';
+  sections = [
+    { title: 'Inicio', selector: 'home' },
+    { title: 'Acerca de Mí', selector: 'about' },
+    { title: 'Proyectos', selector: 'projects' },
+    { title: 'Contacto', selector: 'contact' },
+  ];
+
+  scrollToSection(selector: string): void {
+    const section = document.getElementById(selector);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
